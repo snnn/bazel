2f861ac169fa20b1f080ec32f6031b96c9f7237d
Changming Sun <chasun@microsoft.com>
2017-11-17 17:37:05 +0800

    CI: add beta to version string

--- ci_build.py
+++ ci_build.py
@@ -26,7 +26,7 @@ print('bazel version: %s' % bazel_version)
 
 OLD_BAZEL = '%s\\packages\\bazel.0.6.0.3433\\tools\\bazel.exe' % args.build_binaries_directory
 print('start build,old bazel=%s' % OLD_BAZEL)
-subprocess.check_call([OLD_BAZEL, '--batch', 'build',('--embed_label=%s' % bazel_version),'--stamp','--features','generate_pdb_file','--config','opt','--action_env=NO_MSVC_WRAPPER=1','--color=no','--compilation_mode','opt','--verbose_failures','--experimental_ui','--copt=/Z7','--host_copt=/Z7','--copt=/Oi','--host_copt=/Oi','--linkopt=/DEBUG:FULL','--copt=/DNDEBUG','--host_copt=/DNDEBUG','src:bazel.exe'])
+subprocess.check_call([OLD_BAZEL, '--batch', 'build',('--embed_label=%s-beta' % bazel_version),'--stamp','--features','generate_pdb_file','--config','opt','--action_env=NO_MSVC_WRAPPER=1','--color=no','--compilation_mode','opt','--verbose_failures','--experimental_ui','--copt=/Z7','--host_copt=/Z7','--copt=/Oi','--host_copt=/Oi','--linkopt=/DEBUG:FULL','--copt=/DNDEBUG','--host_copt=/DNDEBUG','src:bazel.exe'])
 print('build finished')
 
 if os.path.exists('nuget_package'):
@@ -40,7 +40,7 @@ shutil.copyfile(('%s\\bazel-bin\\src\\bazel.exe' % args.root),'nuget_package\\to
 root = ET.Element('package')
 metadata = ET.SubElement(root, 'metadata')
 ET.SubElement(metadata, 'id').text = 'Bazel'
-ET.SubElement(metadata, 'version').text = '%s.%d' % (bazel_version, args.buildid)
+ET.SubElement(metadata, 'version').text = '%s.%d-beta' % (bazel_version, args.buildid)
 ET.SubElement(metadata, 'authors').text = 'chasun'
 ET.SubElement(metadata, 'owners').text = 'chasun'
 ET.SubElement(metadata, 'licenseUrl').text = 'https://raw.githubusercontent.com/bazelbuild/bazel/master/LICENSE'
